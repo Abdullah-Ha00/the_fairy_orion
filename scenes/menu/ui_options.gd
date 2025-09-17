57543022d285442edbd2ccfb264e6a7ea8e2afe1
abdallah <abdallahhamze982@gmail.com>
2025-09-17 20:17:18 +0300

    test audio sliders

--- scenes/menu/ui_options.gd
+++ scenes/menu/ui_options.gd
@@ -1,6 +1,18 @@
 extends Node2D
+func _ready() -> void:
+	print($Music/Slider.value)
+	$AudioMusic.play()
 
-	
 
-func _on_button_pressed() -> void:
-	print("tuj")
+func _on_back_pressed() -> void:
+	$AudioSFX.play()
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"),linear_to_db(value))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"),value<0.05)
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"),linear_to_db(value))
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"),value<0.05)
