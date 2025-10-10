efd4784c591c0c75060829e3d3b505221b4ffe37
abdallah <abdallahhamze982@gmail.com>
2025-10-10 18:14:13 +0300

    add monster attack area

--- scenes/monster/elite_monster.gd
+++ scenes/monster/elite_monster.gd
@@ -1,1 +1,16 @@
 extends Monster
+var in_range:bool = false
+
+
+func emit_orb():
+	if in_range:
+		super()
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	if body.is_ally:
+		in_range = true
+
+
+func _on_attack_range_body_exited(_body: Node2D) -> void:
+	in_range = false
+	
