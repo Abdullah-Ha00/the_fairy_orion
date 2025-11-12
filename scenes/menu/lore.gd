9ba6947884dd0da5452410e290184dd49992214d
abdallah <abdallahhamze982@gmail.com>
2025-11-12 13:28:04 +0200

    add collected scroll description

--- scenes/menu/lore.gd
+++ scenes/menu/lore.gd
@@ -11,15 +11,10 @@ func scroll_mouse_entered():
 		scroll.mouse_entered.connect(_on_mouse_entered.bind(scroll))
 
 func _on_mouse_entered(scroll:Area2D):
-	match scroll.name:
-		"Scroll":
-			$ScrollDescription.text = "Found in \nBattle 1"
-		"Scroll2":
-			$ScrollDescription.text = "Found in \nBattle 2"
-		"Scroll3":
-			$ScrollDescription.text = "Score 800 points in \nBattle 2"
-		"Scroll4", "Scroll5":
-			$ScrollDescription.text = "???"
+	if Scrolls.scrolls[scroll.name] == "collected":
+		match_scroll_collected(scroll)
+	else:
+		match_scroll_locked(scroll)
 
 func scroll_mouse_exited():
 	for scroll in get_tree().get_nodes_in_group("Scroll"):
@@ -34,4 +29,28 @@ func enable_scroll():
 			scroll.modulate = Color(255,255,255)
 		else:
 			pass
+
+func match_scroll_locked(scroll:Area2D):
+	match scroll.name:
+		"Scroll":
+			$ScrollDescription.text = "Found in \nBattle 1"
+		"Scroll2":
+			$ScrollDescription.text = "Found in \nBattle 2"
+		"Scroll3":
+			$ScrollDescription.text = "Score 800 points in \nBattle 2"
+		"Scroll4", "Scroll5":
+			$ScrollDescription.text = "???"
+			
+func match_scroll_collected(scroll:Area2D):
+	match scroll.name:
+		"Scroll":
+			$ScrollDescription.text = "Tajtuj"
+		"Scroll2":
+			$ScrollDescription.text = "rara"
+		"Scroll3":
+			$ScrollDescription.text = "asjkld"
+		"Scroll4":
+			$ScrollDescription.text = "Mario et Luigi"
+		"Scroll5":
+			$ScrollDescription.text = "Mario et Luigi 2"
 	
